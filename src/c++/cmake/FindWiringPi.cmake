8bf9e1b200f60c46ba997872a89cfc1e9af08611
Nick Rizzo <rizzo.n@northeastern.edu>
2020-12-07 19:28:12 -0500

    [fix] FindWiringPi never triggering isntall script

--- src/c++/cmake/FindWiringPi.cmake
+++ src/c++/cmake/FindWiringPi.cmake
@@ -4,17 +4,22 @@
 # WiringPi_LIBS         - Location of WiringPi bins
 # WiringPi_INCLUDE_DIRS - Location of the WiringPi header
 
+# paths come from WiringPi ./build script (called from setup script)
+find_library(WiringPi_LIBS wiringPi)
+find_path(WiringPi_INCLUDE_DIRS wiringPi.h)
+
 # check if not found (need to call build script)
-if (NOT EXISTS wiringPi.h OR NOT EXISTS wiringPi)
+if (NOT WiringPi_INCLUDE_DIRS)
     message(WARNING "WiringPi library not found -- calling build script")
     execute_process(
         # call from helper directly to avoid using sudo
         COMMAND ${CMAKE_SOURCE_DIR}/install/helpers/WiringPi.sh --mode install
     )
+    # try to find again now that it was installed
+    find_library(WiringPi_LIBS NAMES wiringPi)
+    find_path(WiringPi_INCLUDE_DIRS NAMES wiringPi.h)
 endif()
 
-# paths come from WiringPi ./build script (called from setup script)
-find_library(WiringPi_LIBS NAMES wiringPi)
-find_path(WiringPi_INCLUDE_DIRS NAMES wiringPi.h)
+
 
 MARK_AS_ADVANCED(WiringPi_LIBS WiringPi_INCLUDE_DIRS)
