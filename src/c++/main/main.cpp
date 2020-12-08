0572a54de569983b6e932b9929cb39620bf5cf04
Nick Rizzo <rizzo.n@northeastern.edu>
2020-12-08 15:46:26 -0500

    [fix] Working way to handle signal

--- src/c++/main/main.cpp
+++ src/c++/main/main.cpp
@@ -11,14 +11,15 @@ using std::cerr;
 using std::endl;
 
 int main() {
-    // create gpio obj to controll rpi
-    gpio::GPIO_Controller gpio_obj;
+    // create single static gpio obj to controll rpi
+    // static needed so it can be accessed in lambda
+    static gpio::GPIO_Controller gpio_obj;
 
     // setup ctrl+c handler w/ callback to stop threads
-    create_signal_handler_callback(std::function<void()>([&]() {
-            gpio_obj.setShouldThreadExit(true);
-        })
-    );
+    std::signal(SIGINT, [](int signum) {
+        cout << "Caught ctrl+c: " << signum << endl;
+        gpio_obj.setShouldThreadExit(true);
+    });
 
     cout << "Blinking red led" << endl;
     gpio_obj.blinkLEDs({"red", "green"});
