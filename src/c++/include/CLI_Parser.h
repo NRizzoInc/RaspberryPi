9cd54ad442ce643335392d194ff712222b571f8e
Nick Rizzo <rizzo.n@northeastern.edu>
2020-12-08 19:18:46 -0500

    [feat] Added skeleton for cli parser

--- src/c++/include/CLI_Parser.h
+++ src/c++/include/CLI_Parser.h
@@ -32,19 +32,6 @@ class CLI_Parser : public CLI::App {
             const std::string name="GPIO CLI Parser"
         );
 
-        /**
-         * @Brief: Parses command line flags and returns results in json"
-         * @Args:
-         *      name: the name of the parser
-         *      parser_description: Description of the parser
-         *      argc: The number of arguments
-         *      argv: The arguments
-         * @Return: json containing collated flag results. May need to convert "values" using .get<T>() on elements
-         * @Note: These arguments should be taken directly from int main(argc, argv)
-         * @Note: using CLI11 for command-line parsing: https://cliutils.gitlab.io/CLI11Tutorial/
-         */
-        // nlohmann::json parse_flags(int argc, char* argv[], std::string parser_name) const;
-
         /********************************************* Parse Functions *********************************************/
 
         /********************************************* Getters/Setters *********************************************/
