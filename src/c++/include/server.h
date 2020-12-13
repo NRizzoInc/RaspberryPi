ea735a3d12f723206adc730101437a789ca1f0c7
Nick Rizzo <rizzo.n@northeastern.edu>
2020-12-12 21:25:49 -0500

    [feat] Added stub code for server

---
+++ src/c++/include/server.h
@@ -0,0 +1,35 @@
+#ifndef RPI_SERVER_H
+#define RPI_SERVER_H
+
+// Standard Includes
+#include <iostream>
+#include <string>
+
+// Our Includes
+
+// 3rd Party Includes
+
+namespace RPI {
+
+class Server {
+    public:
+        /********************************************** Constructors **********************************************/
+        Server();
+        virtual ~Server();
+
+        /********************************************* Getters/Setters *********************************************/
+
+        /********************************************* Server Functions ********************************************/
+
+
+    private:
+        /******************************************** Private Variables ********************************************/
+
+        /********************************************* Helper Functions ********************************************/
+
+}; // end of server class
+
+
+} // end of RPI namespace
+
+#endif
