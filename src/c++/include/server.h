0657232b15edd6c6506363b193d4ebc982fb54a0
Nick Rizzo <rizzo.n@northeastern.edu>
2020-12-12 21:44:07 -0500

    [feat] Added some basic ingredients for server class

--- src/c++/include/server.h
+++ src/c++/include/server.h
@@ -18,7 +18,12 @@ namespace RPI {
 class TcpServer {
     public:
         /********************************************** Constructors **********************************************/
-        TcpServer();
+
+        /**
+         * @brief Construct a new Tcp Server object
+         * @param port_num The port number for the listen port
+         */
+        TcpServer(const int port_num);
         virtual ~TcpServer();
 
         /********************************************* Getters/Setters *********************************************/
@@ -28,6 +33,12 @@ class TcpServer {
 
     private:
         /******************************************** Private Variables ********************************************/
+
+        int listen_sock_fd;     // tcp socket file descriptor to wait to accept connections with client
+        int data_sock_fd;       // tcp socket file descriptor to communciate data with client
+        std::string ip_addr;    // ip address of connected client
+        int listen_port;        // port number of client
+        bool end_conn;          // true if should exit/stop connection
 
         /********************************************* Helper Functions ********************************************/
 
--- src/c++/server/server.cpp
+++ src/c++/server/server.cpp
@@ -5,7 +5,13 @@ namespace RPI {
 
 /********************************************** Constructors **********************************************/
 
-TcpServer::TcpServer() {
+TcpServer::TcpServer(const int port_num)
+    : listen_sock_fd{-1}            // init to invalid
+    , data_sock_fd{-1}              // init to invalid
+    , ip_addr{}                     // empty string bc no client yet
+    , listen_port{port_num}         // wait to accept connections at this port
+    , end_conn{false}               // not ready to end connection yet
+{
     // stub
 }
 
