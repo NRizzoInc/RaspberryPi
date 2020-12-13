f24dcaabef3530b8d9bbbc903b4f1620c5851c81
Nick Rizzo <rizzo.n@northeastern.edu>
2020-12-13 01:43:55 -0500

    [feat] generic write/read functions

--- src/c++/include/packet.h
+++ src/c++/include/packet.h
@@ -1,7 +1,22 @@
 #ifndef PACKET_H
 #define PACKET_H
 
+// Standard Includes
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+// Our Includes
+
+// 3rd Party Includes
+#include <json.hpp>
+
 namespace Network {
+
+// for convenience inside Network namespace
+using json = nlohmann::json;
+using bson = std::vector<std::uint8_t>;
 
 /**
  * @brief This class is responsible for writing and reading packets
@@ -16,6 +31,21 @@ class Packet {
         /********************************************* Getters/Setters *********************************************/
 
         /*************************************** Packet Read/Write Functions ***************************************/
+        // see https://github.com/nlohmann/json#binary-formats-bson-cbor-messagepack-and-ubjson
+
+        /**
+         * @brief Interprets a received char buffer packet (stringified json)
+         * @param pkt_buf A char array containing a stringified json
+         * @return The packet in json form
+         */
+        json readPkt(const char* pkt_buf) const;
+
+        /**
+         * @brief Construct & serialize a json packet
+         * @param pkt_to_send The json packet to send
+         * @return The serialized char array to send
+         */
+        const char* writePkt(const json pkt_to_send) const;
 
     private:
         /******************************************** Private Variables ********************************************/
--- src/c++/server/packet.cpp
+++ src/c++/server/packet.cpp
@@ -16,6 +16,16 @@ Packet::~Packet() {
 
 /*************************************** Packet Read/Write Functions ***************************************/
 
+json Packet::readPkt(const char* pkt_buf) const {
+    return json::from_bson(pkt_buf);
+}
+
+const char* Packet::writePkt(const json pkt_to_send) const {
+    // std::vector<std::uint8_t> == const char*
+    const bson pkt {json::to_bson(pkt_to_send)};
+    return reinterpret_cast<const char*>(pkt.data());
+}
+
 
 /********************************************* Helper Functions ********************************************/
 
