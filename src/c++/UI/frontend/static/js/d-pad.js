8307176f8dd7043dde65136d3a85ae11a6339ae3
Nick Rizzo <rizzo.n@northeastern.edu>
2020-12-20 22:41:24 -0500

    [fix] d-pad controller now accepts wasd keys

--- src/c++/UI/frontend/static/js/d-pad.js
+++ src/c++/UI/frontend/static/js/d-pad.js
@@ -7,8 +7,6 @@ document.body.addEventListener("click", e => {
     if (targetElement && targetElement.nodeName == "A") {
         e.preventDefault();
     }
-    // TODO: remove after done debugging
-    console.log(`Triggered: ${targetName}`)
 });
 
 let dpads = Array.prototype.slice.call(document.getElementsByClassName('d-pad'), 0)
@@ -18,26 +16,31 @@ let els = dpads.concat(opads);
 // handles the pressing of a button
 const press = (press_direction) => {
     for (let i = 0; i < els.length; i++) {
-        const el = els[i],
-            d = el.className.indexOf('d-') !== -1,
-            what = d ? 'd-pad' : 'o-pad';
-        console.log(what);
-        el.className = what + ' ' + press_direction;
+        let el = els[i]
+        const ctrl_idx = el.className.indexOf('d-') !== -1
+        const which_ctrler = ctrl_idx ? 'd-pad' : 'o-pad'
+        // TODO: remove after debugging
+        console.log(`${which_ctrler}: ${press_direction}`);
     }
 }
 
+// keyCode list: https://keycode.info/
 document.body.onkeyup = (e) => {
-    switch (e.which) {
-        case 37:
+    switch (e.keyCode) {
+        case 65: // 'a' key
+        case 37: // left arrow key
             press('left');
             break;
-        case 39:
+        case 68: // 'd' key
+        case 39: // right arrow key
             press('right');
             break;
-        case 38:
+        case 87: // 'w' key
+        case 38: // up arrow key
             press('up');
             break;
-        case 40:
+        case 83: // 's' key
+        case 40: // down arrow key
             press('down');
             break;
     }
