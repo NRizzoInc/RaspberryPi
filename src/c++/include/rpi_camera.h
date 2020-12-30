353214be3d799cc6909a5e79d56b4e63b498a796
Nick Rizzo <rizzo.n@northeastern.edu>
2020-12-29 20:43:56 -0500

    [feat] Added files for rpi camera code

---
+++ src/c++/include/rpi_camera.h
@@ -0,0 +1,24 @@
+#ifndef RPI_CAMERA_H
+#define RPI_CAMERA_H
+
+// Standard Includes
+#include <iostream>
+#include <string>
+
+// Our Includes
+#include "constants.h"
+
+// 3rd Party Includes
+
+namespace RPI {
+
+namespace Camera {
+
+
+
+}; // end of Camera namespace
+
+}; // end of RPI namespace
+
+
+#endif
