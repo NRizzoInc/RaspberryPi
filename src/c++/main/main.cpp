cb30aa46242386b9da9aed7c8314efad66554427
Nick Rizzo <rizzo.n@northeastern.edu>
2021-01-03 02:56:18 -0500

    [rm] useless print

--- src/c++/main/main.cpp
+++ src/c++/main/main.cpp
@@ -158,7 +158,6 @@ int main(int argc, char* argv[]) {
 
     // startup client or server in a thread
     if (is_net) {
-        cout << "Started net agent" << endl;
         net_agent->runNetAgent(false);
     }
 
