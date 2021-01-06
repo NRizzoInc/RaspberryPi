05b75c216a113b06055eb62ddb5e93c4b98b281e
Nick Rizzo <rizzo.n@northeastern.edu>
2021-01-06 18:15:30 -0500

    [feat] stub files for servo controller

---
+++ src/c++/gpio/Servo_Controller.cpp
@@ -0,0 +1,15 @@
+#include "Servo_Controller.h"
+
+namespace RPI {
+namespace gpio {
+namespace Servo {
+
+using std::cout;
+using std::cerr;
+using std::endl;
+
+
+
+}; // end of Servo namespace
+}; // end of gpio namespace
+}; // end of RPI namespace
---
+++ src/c++/include/Servo_Controller.h
@@ -0,0 +1,23 @@
+#ifndef SERVO_CONTROLLER_H
+#define SERVO_CONTROLLER_H
+
+// Standard Includes
+#include <iostream>
+#include <string>
+
+// Our Includes
+#include "constants.h"
+
+// 3rd Party Includes
+
+namespace RPI {
+namespace gpio {
+namespace Servo {
+
+
+}; // end of Servo namespace
+}; // end of gpio namespace
+}; // end of RPI namespace
+
+
+#endif
