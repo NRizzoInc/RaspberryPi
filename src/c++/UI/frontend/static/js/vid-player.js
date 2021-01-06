1b683be53a7154f28044aaa264f6d96088968dc8
Nick Rizzo <rizzo.n@northeastern.edu>
2021-01-06 01:48:18 -0500

    [refactor][ui][feat] if backend dies, page will pause & wait until back alive and resume cleanly

--- src/c++/UI/frontend/static/js/vid-player.js
+++ src/c++/UI/frontend/static/js/vid-player.js
@@ -66,8 +66,6 @@ $("document").ready( async () => {
         // update backend with camera status
         sendPkt({}, {}, camera_status)
     }
-    // trigger toggle for starting condition to show pause button while removing blue box
-    playpause_click(true)
 
     /**
      * @brief on click callback that JUST toggles play button
@@ -75,35 +73,95 @@ $("document").ready( async () => {
      */
     const toggle_playpause_btn = () => playpause_click(null)
 
+    
+    let isCamStopped = false
+    let WakeupInterval = null // stores reference to interval that checks if page is up
+    // loops and checks if page has come back up yet, if so, it start everything up
+    const CreateWakeupInterval = () => {
+        // only create interval if dne
+        if (WakeupInterval != null) return
+
+        WakeupInterval = setInterval( 
+            async () => {
+                // if camera is currently going, there is nothing to do
+                if (!isCamStopped) return
+
+                // if camera currently stopped, but the page is back up, then restart
+                else if (await doesPageExist(cam_original_src, "GET")) {
+                    StartCamActivities()
+                    clearInterval(WakeupInterval)
+                    WakeupInterval = null
+                }
+            }, 3000 // check every 3 seconds
+        )
+    }
+
+    // responsible for stopping all listeners and intervals working on behalf of the camera/video
+    const StopCamActivites = () => {
+        // dont stop if already stopped
+        if (isCamStopped) return
+
+        // stop click event listeners
+        stop_dict.listeners.forEach(type_el => type_el[1].removeEventListener(type_el[0], toggle_playpause_btn))
+
+        // set play button to is paused
+        playpause_click(false)
+
+        // https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/removeEventListener
+        stop_dict.intervals.forEach(int_pid => clearInterval(int_pid))
+
+        // reset stop lists
+        // fast and efficient way to clear array: https://stackoverflow.com/a/1232046/13933174
+        stop_dict.listeners.length = 0
+        stop_dict.intervals.length = 0
+        isCamStopped = true
+
+        // start up wakeup routine to wait for moment to restart
+        CreateWakeupInterval()
+    }
+
+    // creates the interval which continously grabs frames to generate the "video"
+    // wrap in function so interval can be recreated when/if stopped
     // keep reloading JUST the image at the correct fps to mimic a video
-    // if connection dies, stop trying to reload
-    stop_dict.intervals.push(setInterval(
-        async () => {
-            // only refresh image if backend is still up
-            // -- prevents issue with reloading to blank image and erroring
-            const src_exist = await doesPageExist(cam_original_src, "GET")
-            if (!src_exist) {
-                // stop click event listeners
-                stop_dict.listeners.forEach(type_el => type_el[1].removeEventListener(type_el[0], toggle_playpause_btn))
+    const CreateVidStream = () => {
+        // trigger toggle for starting condition to show pause button while removing blue box
+        playpause_click(true)
 
-                // set play button to is paused
-                playpause_click(false)
+        stop_dict.intervals.push(setInterval(
+            async () => {
+                // if connection dies, dont try to reload image (keep last)
+                // only refresh image if backend is still up
+                // -- prevents issue with reloading to blank image and erroring
+                const src_exist = await doesPageExist(cam_original_src, "GET")
+                if (!src_exist) {
+                    StopCamActivites()
+                    return // prevent latest frame from reloading
+                }
 
-                // https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/removeEventListener
-                stop_dict.intervals.forEach(int_pid => clearInterval(int_pid))
-                return // prevent latest frame from reloading
-            }
+                // attach random string to force reload of JUST the image
+                const cache_refresh = `?v=${new Date().getTime()}`
+                cam_vid.src = cam_original_src + cache_refresh
+            }, 1000 / fps // need ms per frame
+        ))
+    }
 
-            // attach random string to force reload of JUST the image
-            const cache_refresh = `?v=${new Date().getTime()}`
-            cam_vid.src = cam_original_src + cache_refresh
-        }, 1000 / fps // need ms per frame
-    ))
+    // responsible for creating btn listeners in regards to the camera/video
+    const CreateCamBtnListners = () => {
+        // play/pause on clicking image or play/pause btn
+        play_pause_els.forEach( (btn_el) => {
+            // actually create onclick event listener & store it for eventual removal
+            btn_el.addEventListener("click", toggle_playpause_btn)
+            stop_dict.listeners.push(["click", btn_el])
+        })
+    }
 
-    // play/pause on clicking image or play/pause btn
-    play_pause_els.forEach( (btn_el) => {
-        // actually create onclick event listener & store it for eventual removal
-        btn_el.addEventListener("click", toggle_playpause_btn)
-        stop_dict.listeners.push(["click", btn_el])
-    })
+    // responsible for starting all listeners and intervals working on behalf of the camera/video
+    const StartCamActivities = () => {
+        CreateVidStream()
+        CreateCamBtnListners()
+        isCamStopped = false
+    }
+
+    // start up page to begin with
+    StartCamActivities()
 })
